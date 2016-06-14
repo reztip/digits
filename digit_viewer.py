004dab777cb4556585061dde63eadbf53946a1ba
reztip <alexpitzer42@gmail.com>
2016-06-13 21:59:50 -0700

    refactored image showing code to file

---
+++ digit_viewer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Take a single line pandas dataframe row of length n^2, 
+# return n x n numpy array
+
+def reshape_image(row_number, dframe, labels):
+    data_row = dframe[row_number:(row_number + 1)].values[0]
+    n = np.sqrt(data_row.size).astype(np.int)
+    img = list()
+    for i in range(n):
+        row = np.array(data_row[i * n : (i + 1) * n])
+        img.append(row)
+    img = np.array(img)
+    return img
+
+def get_label(row_number, labels):
+    return labels[row_number]
+
+def print_image(img, label):
+    plt.imshow(img)
+    plt.title('Image of A Digit - Labeled as {0}'.format(label),\
+            fontweight = 'bold', fontsize = 16)
+    plt.show()
