2b94a888dc1a9f3055cf842257f5fdc44eca9aa5
reztip <alexpitzer42@gmail.com>
2016-06-18 14:08:51 -0700

    hyperparam optimization for SVM classifier

--- summary_utils.py
+++ summary_utils.py
@@ -18,19 +18,22 @@ def avg_img(full_dframe, labelnum):
     avg_of_class = average_of_class(grouped, labelnum)
     return reshape_image_by_row(avg_of_class)
 
-def score_classifier(y_true, y_pred):
-    acc = accuracy_score(y_true, y_pred)
+def score_classifier(y_true, y_pred, method = accuracy_score, ds = 'Training'):
+    acc = method(y_true, y_pred)
+    print(ds)
     print('-' * 30)
     print("The score on the data set is {:3.1f}%".\
      format(acc *  100)) 
     print('-' * 30)
     return acc
 
-
-
-
-
-
+# Fits and scores a classifier on both test and train set
+# Using the accuracy_score
+def fit_and_score(clf, x_train, y_train, x_test, y_test):
+    clf.fit(x_train, y_train)
+    s1 = score_classifier(y_train, clf.predict(x_train))
+    s2 = score_classifier(y_test, clf.predict(x_test), ds = "Testing")
+    return (s1, s2)
 
 
 
