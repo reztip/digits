05c3c1f7f2bbafcbe2e134f4567b143b1a78a27a
reztip <alexpitzer42@gmail.com>
2016-06-18 11:20:14 -0700

    add in feature selection to the randomforestclassifier work

--- summary_utils.py
+++ summary_utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from digit_viewer import *
-
+from sklearn.metrics import accuracy_score
 
 def average_of_classes(full_dframe):
     grouped = full_dframe.groupby('label').aggregate(np.mean).\
@@ -18,9 +18,13 @@ def avg_img(full_dframe, labelnum):
     avg_of_class = average_of_class(grouped, labelnum)
     return reshape_image_by_row(avg_of_class)
 
-def score_classifier(clf, X_train, y_train, X_test, y_test):
-    print("The score on the training set is {:3.1f}%, and on the testing set is {:3.1f}%.".\
-            format(clf.score(X_train, y_train), clf.score(X_test, y_test) * 100)) 
+def score_classifier(y_true, y_pred):
+    acc = accuracy_score(y_true, y_pred)
+    print('-' * 30)
+    print("The score on the data set is {:3.1f}%".\
+     format(acc *  100)) 
+    print('-' * 30)
+    return acc
 
 
 
